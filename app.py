9997f09311489f0e25d14e615e2813e78d1f003b
Bart Wille <bartp.wille@gmail.com>
2018-11-06 13:27:46 +0100

    more flask

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 # "application.py" is the common name for Flask
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import datetime
 
 # create a new flask app
 app = Flask(__name__)
@@ -10,10 +11,31 @@ app = Flask(__name__)
 # when the user goes to my main page (with just slash) the run the index function
 @app.route("/")
 def main():
-    headline = "var test"
-    return render_template("main.html", headline=headline)
+    now = datetime.datetime.now()
+    # create a boolean variable which is True when 01.01
+    new_year = now.month == 1 and now.day == 1
+    return render_template("main.html", new_year=new_year)
 
-@app.route("/<string:name>")
+# Test 1
+@app.route("/t1_<string:name>")
 def hello(name):
     zin = "<h1>hi {}</h1>".format(name)
     return zin
+
+# Test 2
+@app.route("/t2")
+def list_names():
+    names = ["Bart", "Roksana", "Kurwaman"]
+    return render_template("main.html", names=names, new_year=False)
+
+# Test 3
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+# Test 4
+@app.route("/about", methods=["POST"])
+def test4():
+    test = request.form.get("test")
+    test = test
+    return render_template("test4.html", test=test)
