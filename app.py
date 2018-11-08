d83899ec78d7fd9edb0243aeee1073b8291db286
Bart Wille <bartp.wille@gmail.com>
2018-11-08 11:32:05 +0100

    random quotes

--- app.py
+++ app.py
@@ -3,55 +3,69 @@
 from flask import Flask, render_template, request, session
 from flask_session import Session
 import datetime
+from random import randrange
 
 # create a new flask app
 app = Flask(__name__)
 
-# route: part of the URL which determines which pages you request
-# slash is the default page of the website
-# when the user goes to my main page (with just slash) the run the index function
-@app.route("/")
-def main():
-    now = datetime.datetime.now()
-    # create a boolean variable which is True when 01.01
-    new_year = now.month == 1 and now.day == 1
-    return render_template("main.html", new_year=new_year)
-
-# Test 1
-@app.route("/t1_<string:name>")
-def hello(name):
-    zin = "<h1>hi {}</h1>".format(name)
-    return zin
-
-# Test 2
-@app.route("/t2")
-def list_names():
-    names = ["Bart", "Roksana", "Kurwaman"]
-    return render_template("main.html", names=names, new_year=False)
-
-# Test 3
-@app.route("/about")
-def about():
-    return render_template("about.html")
-
-# # Test 4
-# @app.route("/about", methods=["POST"])
-# def test4():
-#     test = request.form.get("test")
-#     test = test
-#     return render_template("test4.html", test=test, mylist=mylist)
-
-# Test 5
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-@app.route("/about", methods=["GET", "POST"])
-def test5():
-    if session.get("mylist") is None:
-        session["mylist"] = []
-    if request.method == "POST":
-        test = request.form.get("test")
-        session["mylist"].append(test)
+# route: part of the URL which determines which pages you request
+# slash is the default page of the website
+# when the user goes to my main page (with just slash) the run the index function
 
-    return render_template("about.html", test=test, mylist=session["mylist"])
+quotes = ["I can't believe you committed suicide. I cannot believe you committed suicide. How could you have done this? How could you have committed suicide?",
+"I'm going to continue hacking into these government systems to see what I can find out.",
+"Here are the files and supporting documents. And supporting truths. The factual documents.",
+"I've been hacking into government and corporate systems all over the country. All over the world. I have discovered more information than any hacker ever has.",
+"I don't know who that is!"]
+
+# Test 3
+@app.route("/about")
+def about():
+    i = randrange(len(quotes)-1)
+    quote = quotes[i]
+    return render_template("about.html", quote=quote)
+
+
+# --------------------TESTS-----------------------
+# @app.route("/t2")
+# def list_names():
+#     names = ["Bart", "Roksana", "Kurwaman"]
+#     return render_template("main.html", names=names, new_year=False)
+#
+# @app.route("/t4")
+# def get_t4():
+#     return render_template("test4.html")
+#
+# @app.route("/t3", methods=["POST"])
+# def test4():
+#     test = request.form.get("test")
+#     test = test
+#     return render_template("test4.html", test=test, mylist=mylist)
+#
+# @app.route("/t5", methods=["GET", "POST"])
+# def test5():
+#     if session.get("mylist") is None:
+#         session["mylist"] = []
+#     if request.method == "POST":
+#         test = request.form.get("test")
+#         session["mylist"].append(test)
+#
+#     return render_template("about.html", test=test, mylist=session["mylist"])
+#
+# @app.route("/t6")
+# def main():
+#     now = datetime.datetime.now()
+#     # create a boolean variable which is True when 01.01
+#     new_year = now.month == 1 and now.day == 1
+#     return render_template("main.html", new_year=new_year)
+# --------------------AFVAL-----------------------
+
+# # Test 1
+# @app.route("/t1_<string:name>")
+# def hello(name):
+#     zin = "<h1>hi {}</h1>".format(name)
+#     return zin
